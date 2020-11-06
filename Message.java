3ea3f0be315893b24767e7bba1dbe5331c2832de
William Croslen <wdcroslen@miners.utep.edu>
2020-11-05 18:24:21 -0700

    setters/getters

--- Message.java
+++ Message.java
@@ -1,5 +1,35 @@
 package iOS_Message;
 
 public class Message {
-
+	int month;
+	int day;
+	String time;
+	String timeOfDay;
+	
+	public void setMonth(int month){
+		this.month = month;
+	}
+	public void setDay(int day){
+		this.day = day;
+	}
+	public void setTime(String time){
+		this.time = time;
+	}
+	public void setTimeOfDay(String timeOfDay){
+		this.timeOfDay = timeOfDay;
+	}
+	
+	
+	public int getMonth(){
+		return this.month;
+	}
+	public int getDay(){
+		return this.day;
+	}
+	public String getTime(){
+		return this.time;
+	}
+	public String getTimeOfDay(){
+		return this.timeOfDay;
+	}
 }
