cf0ec3a4c56d5d5706b06855d68f181083865d31
Ruth Trejo <rtrejo9@miners.utep.edu>
2020-11-05 18:35:23 -0700

    Added method skeleton to Notification

--- Notification.java
+++ Notification.java
@@ -1,5 +1,22 @@
 package iOS_Message;
 
 public class Notification {
-
+	public static void main(String [] args) {
+		Contact [] contacts = new Contact[4];
+	}
+	
+	/* The method prints the message of the specific Contact the user chose. */
+	public static void showMessageInfo(Contact [] contacts) {
+		
+	}
+	
+	/* The method prints the Phone Number of the specific Contact the user chose. */
+	public static String showPhoneNumber(Contact [] contacts)) {
+		
+	}
+	
+	/* The method displayed the meny that allows the user to choose. */
+	public static void displayMenu() {
+		
+	}
 }
