a94dac1467817ccd8fe39d37188315835e0516ad
Ruth Trejo <rtrejo9@miners.utep.edu>
2020-11-05 18:59:36 -0700

    edited fillContact method

--- Notification.java
+++ Notification.java
@@ -22,8 +22,41 @@ public class Notification {
 	
 	/* The method will fill the contact array in the main menu with the contact list provided to students. */
 	public static void fillContacts(Contact [] contacts) {
-		for (){
-			
-		}
+		//Using constructor to set Contact and message attributes
+		contacts[0] = new Contact("Erik","Macik","915-211-1234", 
+				new Message(11,03,"05:19","PM","Hey guys look at this meme I found"));
+		contacts[1] = new Contact("Joshua","Ramos","915-987-6547",
+				new Message(11,02,"02:13","AM","Yo! I just won first place in FallGuys! :D"));
+		
+		//Using setters to set Contact and message values
+		
+		//Creating Message and Contact object for William using setters
+		Message william = new Message();
+		william.setText("Draw me like one of french girls ;)");
+		william.setMonth(11);
+		william.setDay(10);
+		william.setTimeOfDay("PM");
+		william.setTime("09:06");
+		
+		contacts[2] = new Contact();
+		contacts[2].setFirstName("William");
+		contacts[2].setLastName("Croslen");
+		contacts[2].setPhoneNumber("915-321-7854");
+		contacts[2].setMessage(william);
+		
+		//Creating Message and Contact object for Ruth using setters
+		Message ruth = new Message();
+		ruth.setText("so... I just wasted another $25 in COD mobile");
+		ruth.setMonth(11);
+		ruth.setDay(03);
+		ruth.time("11:54");
+		ruth.setTimeOfDay("PM");
+		
+		//setting contact information for Ruth
+		contacts[3] = new Contact();
+		contacts[3].setFirstName("Ruth");
+		contacts[3].setLastName("Trejo");
+		contacts[3].setPhoneNumber("915-741-2589");
+		contacts[3].setMessage(ruth);
 	}
 }
